2c89b9db80b7bc98c678b73ea3dbf18ce1714449
pHedi3 <federico.gualdi3@gmail.com>
2021-07-08 17:06:25 +0200

    stampa con qualche bag

--- js/script.js
+++ js/script.js
@@ -4,17 +4,25 @@ Vue.config.devtools = true
 var app = new Vue({
     el: '#app',
     data: {
-        emails: []
+        emails: [],
+        taEmail: []
     },
     created() {
-
+        this.genEmail()
     },
     methods: {
-        callEmail: function () {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    this.emails.push(response.data.response)
-                })
+        genEmail: function () {
+            for (var x = 0; x < 5; x++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    this.taEmail.push(response.data.response)
+                });
+            };
         },
+        stampEmail: function () {
+            if (this.taEmail.length == 5) {
+                this.emails = this.taEmail
+            }
+        }
+
     }
 })
