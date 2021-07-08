d90da2c890d863919dc3d204bf95369e8fe04dce
pHedi3 <federico.gualdi3@gmail.com>
2021-07-08 17:50:54 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ var app = new Vue({
     el: '#app',
     data: {
         emails: [],
-        taEmail: []
     },
     created() {
         this.genEmail()
@@ -14,16 +13,12 @@ var app = new Vue({
         genEmail: function () {
             for (var x = 0; x < 10; x++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                    this.taEmail.push(response.data.response)
+                    this.emails.push(response.data.response)
                 });
             };
         },
         stampEmail: function () {
-            if (this.taEmail.length == 10) {
-                this.emails = this.taEmail
-            } else {
-                this.emails = ''
-            }
+            this.emails = []
             this.genEmail()
         }
 
