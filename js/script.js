80febf0e0c2a49d497c6f50534d629bfd8185e2f
pHedi3 <federico.gualdi3@gmail.com>
2021-07-08 16:06:52 +0200

    genera singola email

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+Vue.config.devtools = true
+
+
 var app = new Vue({
     el: '#app',
     data: {
-        text: 'ciao'
+        emails: []
+    },
+    created() {
+
+    },
+    methods: {
+        callEmail: function () {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emails.push(response.data.response)
+                })
+        },
     }
 })
